47263eb82c8b59fae8b1162fd8d24abd9fa1b9ef
larkinxp <larkinp@duck.com>
2025-08-27 03:28:48 -0400

    updated build names

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mod = b.addModule("htx", .{
+    const mod = b.addModule("html_parser", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
     });
